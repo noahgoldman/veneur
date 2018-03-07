c907855ddbc0f1077f6ad89a4f297db9e7335320
Noah Goldman <noahg34@gmail.com>
2018-03-07 15:57:54 -0800

    Improve forwardtest

--- forwardrpc/forwardtest/server.go
+++ forwardrpc/forwardtest/server.go
@@ -3,6 +3,7 @@ package forwardtest
 import (
 	"context"
 	"net"
+	"sync"
 	"testing"
 
 	"github.com/golang/protobuf/ptypes/empty"
@@ -11,21 +12,36 @@ import (
 	"google.golang.org/grpc"
 )
 
+// SendMetricHandler is a handler that is called when a Server gets a
+// SendMetrics RPC
 type SendMetricHandler func([]*metricpb.Metric)
 
+// Server is a gRPC server similar to httptest.Server
 type Server struct {
-	lis     net.Listener
-	server  *grpc.Server
-	handler SendMetricHandler
+	*grpc.Server
+	lis      net.Listener
+	handler  SendMetricHandler
+	startMtx sync.Mutex
 }
 
+// NewServer creates an unstarted Server with the specified handler
 func NewServer(handler SendMetricHandler) *Server {
-	return &Server{
+	res := &Server{
+		Server:  grpc.NewServer(),
 		handler: handler,
 	}
+
+	forwardrpc.RegisterForwardServer(res.Server, res)
+	return res
 }
 
+// Start starts the gRPC server listening on the loopback interface on a
+// random port.  The address it is listening on can be retrieved from
+// (*Server).Addr()
 func (s *Server) Start(t *testing.T) {
+	s.startMtx.Lock()
+	defer s.startMtx.Unlock()
+
 	var err error
 	s.lis, err = net.Listen("tcp", "127.0.0.1:")
 	if err != nil {
@@ -33,21 +49,21 @@ func (s *Server) Start(t *testing.T) {
 			"server: %v", err)
 	}
 
-	s.server = grpc.NewServer()
-	forwardrpc.RegisterForwardServer(s.server, s)
 	go func() {
-		s.server.Serve(s.lis)
+		s.Serve(s.lis)
 	}()
 }
 
-func (s *Server) Stop() {
-	s.server.Stop()
-}
-
+// Addr returns the address the server is listening on.
 func (s *Server) Addr() net.Addr {
+	s.startMtx.Lock()
+	defer s.startMtx.Unlock()
+
 	return s.lis.Addr()
 }
 
+// SendMetrics calls the input SendMetricsHandler whenever it receives an
+// RPC
 func (s *Server) SendMetrics(ctx context.Context, mlist *forwardrpc.MetricList) (*empty.Empty, error) {
 	s.handler(mlist.Metrics)
 	return &empty.Empty{}, nil
